39e18f6335d45e615f419268180534070eafa4da
rakib <rahmed@hvac.mea.com>
2024-08-28 21:30:33 -0400

    No Icon

--- src/app/components/logo/logo.tsx
+++ src/app/components/logo/logo.tsx
@@ -1,16 +1,16 @@
-import Icon from './icon';
-import { brandName } from '../../../../server';
+import Image from 'next/image';
+import { brandName, icon } from '../../../../server';
 
 export default function Logo({
     fontSize = 24,
-    icon = <Icon />,
     fontWeight = 700,
     label = brandName,
     className = `logo`,
+    src = `/images/logos/${icon}`,
 }: any) {
     return (
         <div className={`logoContainer ${className} flex alignCenter start gap10`} style={{ fontWeight }}>
-            {icon}
+            <Image src={src} alt={`Logo`} width={35} height={26} />
             <strong style={{ fontSize }}>
                 {label}
             </strong>
