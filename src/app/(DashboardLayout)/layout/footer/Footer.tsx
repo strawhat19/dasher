9d9c520906550ccc661894ef07c683d850fc0406
rakib <rahmed@hvac.mea.com>
2024-08-30 21:04:32 -0400

    UI Updates

--- src/app/(DashboardLayout)/layout/footer/Footer.tsx
+++ src/app/(DashboardLayout)/layout/footer/Footer.tsx
@@ -5,17 +5,19 @@ import { Copyright } from '@mui/icons-material';
 
 export default function Footer() {
     return (
-        <Card sx={{ background: `var(--fontColor)` }} className={`footerContainer footerCard`}>
-            <footer className={`footer flex w100 p30 spaceBetween`}>
-                <div className={`leftSide`}>
-                    <Logo className={`footerLogo p0`} />
-                </div>
-                <div className={`rightSide`}>
-                    <div className={`copyright flex gap5`}>
-                        Copyright <Copyright style={{ fontSize: 18, color: `var(--iconRight)` }} /> {year}
+        <div className={`footerComponent`}>
+            <Card sx={{ background: `var(--fontColor)` }} className={`footerContainer footerCard`}>
+                <footer className={`footer flex w100 p30 spaceBetween`}>
+                    <div className={`leftSide`}>
+                        <Logo className={`footerLogo p0`} />
                     </div>
-                </div>
-            </footer>
-        </Card>
+                    <div className={`rightSide`}>
+                        <div className={`copyright flex gap5`}>
+                            Copyright <Copyright style={{ fontSize: 18, color: `var(--iconRight)` }} /> {year}
+                        </div>
+                    </div>
+                </footer>
+            </Card>
+        </div>
     )
 }
